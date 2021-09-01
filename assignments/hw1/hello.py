e8646b7778b94c90c0b47935b0cd4786fbd8aa53
kaiko <orieek@g.cofc.edu>
2021-09-01 13:59:51 -0400

    completed homework 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+
+Name: Derek Kai Oriee
+hello.py
+
+Purpose: This program prints "Hello, world!"
+
+Certificate of Authenticity:
+I certify this assignment is entirely my own work
+"""
+
+def main():
+    print("hello, world!")
+
+main()
