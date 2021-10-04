dfc96697675a39fd500ed1c61963fc9ef549a5be
kaiko <orieek@g.cofc.edu>
2021-10-04 15:16:54 -0400

    WIP HW5

---
+++ assignments/hw5/greeting.py
@@ -0,0 +1,14 @@
+from graphics import *
+
+
+def main():
+    width = 400
+    height = 400
+    win = GraphWin("Greeting", width, height)
+
+    heart =
+
+    win.getMouse()
+    win.close()
+
+main()
