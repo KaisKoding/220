b9070a45206e26b46820e4b653239c23a3cc8440
kaiko <orieek@g.cofc.edu>
2021-10-26 10:43:10 -0400

    HW 7 WIP

---
+++ assignments/hw7/weighted_average.py
@@ -0,0 +1,43 @@
+"""
+Name: Derek Kai Oriee
+
+Certificate of Authenticity:
+I certify this assignment is entirely my own work
+"""
+
+
+def weighted_average(in_file_name, out_file_name):
+    grades_file = open(in_file_name, "r")
+    grades_list = str(grades_file.read())
+    grades_list = grades_list.split("\n")
+
+    class_grade = 0
+    for line in grades_list:
+        split_line = line.split(":")
+        name = split_line[0]
+        numbers = split_line[1].split()
+
+        grades = 0
+        weights = 0
+        for i in range(0, len(numbers), 2):
+            weight = int(numbers[i])
+            grade = int(numbers[i + 1])
+            weights = weights + weight
+            grades = grades + (weight * grade)
+
+        if weights != 100:
+            print("Error: the weights are less than 100.")
+
+        else:
+            avg_file = open(out_file_name, "w")
+            avg = grades / 100
+            avg_file.write(name + ":" + str(avg))
+
+            class_grade = class_grade + avg
+
+
+def main():
+    weighted_average("grades.txt", "avg.txt")
+
+
+main()
